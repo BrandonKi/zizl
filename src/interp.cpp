7250bb200759ddc4a8a81edd6aa260ea63a073d1
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2022-02-10 21:41:54 -0500

    can run test now

--- src/interp.cpp
+++ src/interp.cpp
@@ -16,5 +16,44 @@ void Interp::parse_all_files() {
 }
 
 int Interp::run() {
+    std::vector<u64> stack;
+    for(int i = 0; i < bytecode.size(); ++i) {
+        switch(bytecode[i]) {
+            case ir_add: {
+                auto arg2 = stack.back();
+                stack.pop_back();
+                auto arg1 = stack.back();
+                stack.back() = arg2 + arg1;
+                break;
+            }
+            case ir_sub: {
+                auto arg1 = stack.back();
+                stack.pop_back();
+                auto arg2 = stack.back();
+                stack.back() = arg2 - arg1;
+                break;
+            }
+            case ir_mul:
+            case ir_div:
+            case ir_mod:
+                assert(false);
+            case ir_push: {
+                u64 imm = 0;
+                for(int x = 0; x < 8; ++x)
+                    imm |= bytecode[i + x + 1] << (x * 8);
+                i += 8;
+                stack.push_back(imm);
+                break;
+            }
+            case ir_pop:
+            case ir_dup:
+            case ir_swap:
+
+            case ir_ret:
+                return stack.back();
+            default:
+                assert(false);
+        }
+    }
     return 0;
 }
